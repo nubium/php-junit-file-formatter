0b4ac1c75bbccc2476fda2696c1ea9a1578ddcbe
Adam Valalský <adam.valalsky@nubium.cz>
2023-08-01 14:48:22 +0200

    fix: added phpstan

--- src/FileErrorFormatter.php
+++ src/FileErrorFormatter.php
@@ -8,6 +8,7 @@ use PHPStan\Command\ErrorFormatter\ErrorFormatter;
 use PHPStan\Command\ErrorFormatter\JunitErrorFormatter;
 use PHPStan\Command\ErrorFormatter\TableErrorFormatter;
 use PHPStan\Command\Output;
+use PHPStan\ShouldNotHappenException;
 use Symfony\Component\Console\Output\StreamOutput;
 
 class FileErrorFormatter implements ErrorFormatter
@@ -19,14 +20,25 @@ class FileErrorFormatter implements ErrorFormatter
     )
     {}
 
+    /**
+     * @throws ShouldNotHappenException
+     */
     public function formatErrors(AnalysisResult $analysisResult, Output $output): int
     {
         // write human-readable output to console
         $this->tableErrorFormatter->formatErrors($analysisResult, $output);
 
         // write junit to file
-        $fileStream = new StreamOutput(fopen($this->outputFilePath, 'w'));
+        $resource = fopen($this->outputFilePath, 'w');
+
+        if ($resource === false) {
+            throw new ShouldNotHappenException('Cannot open file for writing: ' . $this->outputFilePath);
+        }
+
+        $fileStream = new StreamOutput($resource);
         $fileOutput = new FileOutput($fileStream, $output->getStyle());
+
+        // @phpstan-ignore-next-line
         $this->junitErrorFormatter->formatErrors($analysisResult, $fileOutput);
 
         return $analysisResult->hasErrors() ? 1 : 0;
